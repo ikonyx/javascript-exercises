3c05352d957e85971597f5a0cc19954c77de0a60
ikram <150953076+ikonyx@users.noreply.github.com>
2024-07-23 14:51:12 +0100

    Add solution to exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(Array, ...otherArgs) {
+
+    // I looped backwards because the splice method 
+      // changes indices everytime it removes an element.
+      // We'd end up skipping the iteration of some elements.
+      for (i=Array.length; i>=0; i--) { 
+      
+        for (let j=0; j<otherArgs.length; j++) {
+          if (Array[i]=== otherArgs[j]) {
+            Array.splice(i,1);
+          }  
+        }
+      }
+      
+      return Array;
+
 };
 
 // Do not edit below this line
