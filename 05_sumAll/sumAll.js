89959728f3863803c72ab9e69e3d57202330fa74
ikram <150953076+ikonyx@users.noreply.github.com>
2024-07-23 15:35:53 +0100

    Add my solution to exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(n, m) {
+    if (n<0 || m<0) { return "ERROR"};
+    if (typeof(n,m) !== "number") { return "ERROR"};
+    if (n != Math.floor(n) || m != Math.floor(m)) { return "ERROR"};
+    if (n>m) { 
+        let p=m;
+        m=n;
+        n=p;
+    }
+
+    
+    let sum =0;
+    for (i=n; i<=m; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
