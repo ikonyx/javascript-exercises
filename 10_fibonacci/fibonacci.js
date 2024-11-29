aca6969fdbd76c6a2d82ec737501cdfce7c3ff54
ikram <150953076+ikonyx@users.noreply.github.com>
2024-11-29 15:24:46 +0100

    Add fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) { return "OOPS"};
 
+    let array = [0,1];
+    for (i=0; i<num; i++) {
+        let n = array.at(-1) + array.at(-2);
+        array.push(n);
+    }
+    return array.at(-2);
 };
 
 // Do not edit below this line
