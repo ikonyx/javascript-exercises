6bca6ec81ef0047cb449e7bdd495cd26efd493e3
ikram <150953076+ikonyx@users.noreply.github.com>
2024-11-29 14:27:50 +0100

    Add palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const newString = string
+    .toLowerCase()
+    .split('')
+    .filter(char => /[^\W_]/.test(char))
+    .join('');
 
+    const reversedString = newString.split('').reverse().join('');
+    return newString === reversedString;
 };
 
 // Do not edit below this line
